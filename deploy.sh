f8ea281a4a5070870216afaa249d0477036ab48d
Justin M. Fear <justin.m.fear@gmail.com>
2019-05-28 16:26:54 -0400

    trying to fix css

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-hugo_ext
+hugo_ext --minify --theme hugo-book
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && git push
